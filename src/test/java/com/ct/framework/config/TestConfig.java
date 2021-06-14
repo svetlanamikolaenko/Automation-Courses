5216d7ec35a023e7a0ff03a89097cacf62c0a08c
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-06-14 18:10:50 +0300

    change config

--- src/test/java/com/ct/framework/config/TestConfig.java
+++ src/test/java/com/ct/framework/config/TestConfig.java
@@ -5,10 +5,12 @@ import org.aeonbits.owner.ConfigFactory;
 
 @Config.Sources("classpath:config.properties")
 public interface TestConfig extends Config {
-    TestConfig CONFIG = ConfigFactory.create(TestConfig.class);
+    TestConfig CONFIG = ConfigFactory.create(TestConfig.class, System.getenv(), System.getProperties());
 
     @Key("browser")
     String browser();
+
+    String browserVersion();
 
     @Key("base.url")
     String baseUrl();
